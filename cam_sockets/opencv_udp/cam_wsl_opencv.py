1bc873f1015dec28e334729627ab2dfa1645749f
Zardosht Hodaie <zhodaie@gmail.com>
2020-11-11 10:46:51 +0100

    Add comments and instructions

--- cam_sockets/opencv_udp/cam_wsl_opencv.py
+++ cam_sockets/opencv_udp/cam_wsl_opencv.py
@@ -11,7 +11,7 @@ def main():
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             
-            cv2.imshow("Video Stram", gray)
+            cv2.imshow("Video Stream", gray)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -25,3 +25,22 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+
+# Run the following command to stream your built-in webcam to the 
+# UDP address udp://127.0.0.1:5000
+# FFMPEG must be installed. You may need to change the device name. 
+# For finding out device names run: 
+# C:\> ffmpeg -list_devices true -f dshow -i dummy
+#
+# To stream run: 
+#
+# C:\> ffmpeg -f dshow -i video="Integrated Camera" -preset ultrafast 
+#             -tune zerolatency -vcodec libx264 -r 10 -b:v 2014k 
+#             -s 640x480 -ab 32k -ar 44100 -f mpegts -flush_packets 0 
+#             udp://127.0.0.1:5000?pkt_size=1316
+#
+# In one line, and with an alternative to make FFMPEG start streaming 
+# faster (has no effect though)
+# C:\> ffmpeg -f dshow -i video="Integrated Webcam" -preset ultrafast -tune zerolatency -vcodec libx264 -r 30 -b:v 2014k -s 640x480 -ab 32k -ar 44100 -f mpegts -flush_packets 0 udp://127.0.0.1:5000?pkt_size=1316
+# C:\>  ffmpeg -f dshow -i video="Integrated Webcam" -preset ultrafast -tune zerolatency -vcodec libx264 -r 30 -b:v 2014k -s 640x480 -ab 32k -ar 44100 -f mpegts -flush_packets 0 -fflags nobuffer udp://127.0.0.1:5000?pkt_size=1316
